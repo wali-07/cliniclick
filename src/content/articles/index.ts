424fc3990e5622ecd6836e0356d2fda612f24615
wali-07 <abdullah.a.wali@gmail.com>
2026-06-22 18:02:57 +0400

    Editorial draft: Cost of HydraFacial in Dubai
    slug: cost-of-hydrafacial-in-dubai
    parent: treatment/hydrafacial
    kind: cost-guide
    cycles: 1/3
    converged: true
    
    Generated by the editorial cron pipeline. Approve via Telegram to merge.

--- src/content/articles/index.ts
+++ src/content/articles/index.ts
@@ -24,6 +24,7 @@ import { whatIsPrp } from "./treatments/prp/what-is-prp";
 import { whatIsChemicalPeels } from "./treatments/chemical-peels/what-is-chemical-peels";
 import { whatIsCoolsculpting } from "./treatments/coolsculpting/what-is-coolsculpting";
 import { costOfMicroneedlingInDubai } from "./treatments/microneedling/cost-of-microneedling-in-dubai";
+import { costOfHydrafacialInDubai } from "./treatments/hydrafacial/cost-of-hydrafacial-in-dubai";
 
 export const allArticles: Article[] = [
   whatIsAcne,
@@ -51,4 +52,5 @@ export const allArticles: Article[] = [
   whatIsChemicalPeels,
   whatIsCoolsculpting,
   costOfMicroneedlingInDubai,
+  costOfHydrafacialInDubai,
 ];
